8cc23580cbfb33375d80f2d2ada483d3bac16dbc
Derek Morr <morr.derek@gmail.com>
2020-01-22 22:45:29 -0500

    address pylint issues: indentation, unused imports, docstrings

--- semantic_search.py
+++ semantic_search.py
@@ -1,17 +1,19 @@
 from io import StringIO
-import sys
-
-#sys.path.append("/usr/home/username/pdfminer")
+import logging
+import os
 
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
 from pdfminer.pdfpage import PDFPage
-import os
-import getopt
+import pandas as pd
+import tensorflow as tf
+import tensorflow_hub as hub
+import spacy
+from sklearn.metrics.pairwise import cosine_similarity
 
-#converts pdf, returns its text content as a string
 def convert(fname, pages=None):
+    "converts pdf, returns its text content as a string"
     if not pages:
         pagenums = set()
     else:
@@ -28,66 +30,47 @@ def convert(fname, pages=None):
     infile.close()
     converter.close()
     text = output.getvalue()
-    output.close
-    return text 
-
-import os
+    output.close()
+    return text
 
 COMPENDIUM_FOLDER_PATH = "/Users/singhcpt/dev/semantic_analysis/test_compendium"
-# COMPENDIUM_FOLDER_PATH = "/Users/singhcpt/Documents/APS_Compendia"
 
 pdf_texts = []
 
 for filename in os.listdir(COMPENDIUM_FOLDER_PATH):
-    if (".pdf" in filename):
-      pdf_texts.append(convert(COMPENDIUM_FOLDER_PATH + "/" + filename))
-
-# from here on is what we need on the VM
-import numpy as np
-import pandas as pd
-import tensorflow as tf
-import tensorflow_hub as hub
-from sklearn import preprocessing
+    if ".pdf" in filename:
+        pdf_texts.append(convert(COMPENDIUM_FOLDER_PATH + "/" + filename))
 
 # python -m spacy download en_core_web_md you will need to install this on first load
-import spacy
-from spacy.lang.en import English
-from spacy import displacy
 nlp = spacy.load('en_core_web_md')
-from IPython.display import HTML
-import logging
 logging.getLogger('tensorflow').disabled = True #OPTIONAL - to disable outputs from Tensorflow
 
 url = "https://tfhub.dev/google/elmo/2"
 embed = hub.Module(url)
 
-import re
-
-text = pdf_texts[0].lower().replace('\n', ' ').replace('\t', ' ').replace('\xa0',' ')
+text = pdf_texts[0].lower().replace('\n', ' ').replace('\t', ' ').replace('\xa0', ' ')
 text = ' '.join(text.split())
 doc = nlp(text)
 
 sentences = []
 for i in doc.sents:
-  if len(i)>1:
-    sentences.append(i.string.strip())
+    if len(i) > 1:
+        sentences.append(i.string.strip())
 
 tf_sentences = tf.reshape(sentences[0:1000], [-1])
 
 embeddings = embed(
-            tf_sentences,
-            signature="default",
-            as_dict=True)["default"]
+    tf_sentences,
+    signature="default",
+    as_dict=True)["default"]
 search_string = "what should i do if my tomato has late blight" #@param {type:"string"}
 print("SEARCH: " + search_string)
-results_returned = "10" #@param [1, 2, 3]
+results_returned = "10"
 
 with tf.Session() as sess:
-  sess.run(tf.global_variables_initializer())
-  sess.run(tf.tables_initializer())
-  x = sess.run(embeddings)
-
-from sklearn.metrics.pairwise import cosine_similarity
+    sess.run(tf.global_variables_initializer())
+    sess.run(tf.tables_initializer())
+    x = sess.run(embeddings)
 
 embeddings2 = embed(
     [search_string],
@@ -95,17 +78,16 @@ embeddings2 = embed(
     as_dict=True)["default"]
 
 with tf.Session() as sess:
-  sess.run(tf.global_variables_initializer())
-  sess.run(tf.tables_initializer())
-  search_vect = sess.run(embeddings2)
-  
+    sess.run(tf.global_variables_initializer())
+    sess.run(tf.tables_initializer())
+    search_vect = sess.run(embeddings2)
 
 cosine_similarities = pd.Series(cosine_similarity(search_vect, x).flatten())
 output = ""
-x = 0
 
 print("RESULTS:")
 
-for i,j in cosine_similarities.nlargest(int(results_returned)).iteritems():
-  x += 1
-  print("RESULT " + str(x) + ": " + sentences[i] + "\n")
+x = 0
+for i, j in cosine_similarities.nlargest(int(results_returned)).iteritems():
+    x += 1
+    print("RESULT " + str(x) + ": " + sentences[i] + "\n")
